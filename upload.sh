a52ed95124fe9b5e340e88b812a7448021cc414a
XinshaoAmosWang <xinshaowang@gmail.com>
2020-07-01 21:04:32 +0100

    Our work motivates an ICML 2020 paper!

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 git remote set-url origin git@github.com:XinshaoAmosWang/Improving-Mean-Absolute-Error-against-CCE.git
 
 git add .
-git commit -m "updates"
+git commit -m "Our work motivates an ICML 2020 paper!"
 git push origin master
